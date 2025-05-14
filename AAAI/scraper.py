c9221f9b11856d1368e4071a5a0d6605351aa110
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2025-05-14 15:34:43 +0200

    AAAI scraper cleanup

--- AAAI/scraper.py
+++ AAAI/scraper.py
@@ -1,19 +1,16 @@
+"""AAAI publication webscraper."""
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import csv
 import sys
 
-
 base = "https://ojs.aaai.org/index.php/AAAI/issue/view/"
-
-# Spoof user agent
-header = {
+header = {  # Spoof user agent
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
 }
 
-# ranges for 2024: 576-596
-
+# Webpage numbers where the publications can be found
 year_ranges = {"2024": list(range(576, 597)),
                "2023": list(range(548, 560)),
                "2022": list(range(507, 514)) + [519, 520, 521],
@@ -34,18 +31,8 @@ target_dir.mkdir(exist_ok=True, parents=True)
 base = "https://ojs.aaai.org/index.php/AAAI/issue/view/"
 
 print("Retrieving meta from: ", base)
-ids = []
-titles = []
-authors = []
-urls = []
-page_numbers = []
-pdf_urls = []
-key_words = []
-author_affiliations = []
-abstracts = []
-how_to_cites = []
 
-# make sure output csv exists
+# Make sure output csv exists
 meta_csv_file = target_dir / "meta.csv"
 print("Saving to: ", meta_csv_file)
 with meta_csv_file.open('a+', newline='') as myfile:
@@ -62,42 +49,32 @@ for index, issue_page in enumerate(year_ranges[year]):
     for div in soup.find_all("div"):
         divclass = div.get("class")
         if divclass is not None and "obj_article_summary" in divclass:
-            paper_links = div.find_all("a")
-            title = paper_links[0].text.strip()
-            titles.append(title)
-            paper_url = paper_links[0]['href']
-            urls.append(urls)
-            pdf_url = paper_links[1]['href']
-            pdf_urls.append(pdf_url)
-            article_id = paper_links[0]['id'].replace("article-", "")
-            ids.append(article_id)
-            print(f"\t({index+1}/{total_pages})[{article_id}] Title: {title}")
-            if article_id in existing_ids:
+            paper_id = paper_links[0]['id'].replace("article-", "")
+            print(f"\t({index+1}/{total_pages})[{paper_id}] Title: {paper_title}")
+            if paper_id in existing_ids:
                 print(f"\tSkipping as it already exists")
+            paper_links = div.find_all("a")
+            paper_title = paper_links[0].text.strip()
+            paper_url = paper_links[0]['href']
+            pdf_url = paper_links[1]['href']
+
+            # Publication specific page for more data
             paper_page = requests.get(paper_url, headers=header)
-            
             paper_soup = BeautifulSoup(paper_page.content, "lxml")
             authors_section = paper_soup.find("section", class_="item authors")
             key_words_section = paper_soup.find("section", class_="item keywords")
             abstract_section = paper_soup.find("section", class_="item abstract")
-            a_names = []
-            a_aff = []
+            author_names = []
+            author_affiliations = []
             for list_item in authors_section.find_all("li"):
                 author_name = list_item.find("span", class_="name").text.strip()
-                a_names.append(author_name)
-                a_affiliation = list_item.find("span", class_="affiliation").text.strip()
-                a_aff.append(a_affiliation)
-            authors.append(a_names)
-            author_affiliations.append(a_aff)
-            key_words_values = [kw.strip() for kw in key_words_section.find("span", class_="value").text.strip().split(",")]
-            key_words.append(key_words_values)
-            abstracts.append(abstract_section.text.strip().replace("\n", " "))
-            how_to_cites.append(paper_soup.find("div", class_="csl-entry").text)
-        else:
-            continue
-        # Write meta data to CSV
-        with meta_csv_file.open('a+', newline='') as myfile:
-            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-            if ids[-1] in existing_ids:
-                continue
-            wr.writerow([ids[-1], titles[-1], authors[-1], author_affiliations[-1], key_words[-1], how_to_cites[-1], abstracts[-1], pdf_urls[-1]])
+                author_names.append(author_name)
+                author_affiliations.append(list_item.find("span", class_="affiliation").text.strip())
+            key_words = [kw.strip() for kw in key_words_section.find("span", class_="value").text.strip().split(",")]
+            abstract_section = abstract_section.text.strip().replace("\n", " ")
+            how_to_cite = paper_soup.find("div", class_="csl-entry").text
+        
+            # Write meta data to CSV
+            with meta_csv_file.open('a+', newline='') as myfile:
+                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                wr.writerow([paper_id, paper_title, author_names, author_affiliations, key_words, how_to_cite, abstract_section, pdf_url])
