b07b116bc914af5cbd0d2ee1f2e1de298c46a684
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2025-09-02 11:46:10 +0200

    scraper update to fix missing URLs

--- ICLR/scraper.py
+++ ICLR/scraper.py
@@ -41,15 +41,24 @@ with meta_csv_file.open('a+', newline='') as myfile:
         wr.writerow(["index", "Title", "Authors", "pdf_url", "project_url", "abstract"])
 
 existing_ids = [l[0] for l in csv.reader(meta_csv_file.open('r'))]
+paper_urls = [l[3] for l in csv.reader(meta_csv_file.open('r'))]
+
+# back off for openreview
+openreview_count = 0
 # Find all paper titles inside <div> tags
 for link_index, l in enumerate(links):
+    resolving = False
     if "accounts/login?nextp=" in l:
         continue
 
     paper_id = str(l.split("/")[-1])
     if paper_id in existing_ids:
-        print(f"Skipping {paper_id} as it already exists")
-        continue
+        if paper_urls[existing_ids.index(paper_id)] == "":
+            print("Trying to resolve missing paper URL...")
+            resolving = True
+        else:
+            print(f"Skipping {paper_id} as it already exists")
+            continue
 
     paper_soup = BeautifulSoup(requests.get(f"{iclr_base}{l}").content, "lxml")
     project_url = ""
@@ -79,11 +88,15 @@ for link_index, l in enumerate(links):
         elif "href" in link.attrs and "openreview" == link.text.lower().strip():
             # Find the implementation link and possibly paper link on openreview
             openreview_url = link.attrs["href"]
-            open_soup = BeautifulSoup(requests.get(openreview_url).content, "lxml")
-            #print(open_soup)
+            if openreview_count >= 30:
+                print("[OpenReview] Limit reached, sleeping for 15 seconds...")
+                import time
+                time.sleep(15)
+                openreview_count = 0
+            request = requests.get(openreview_url)
+            openreview_count += 1
+            open_soup = BeautifulSoup(request.content, "lxml")
             for openlink in open_soup.find_all("a"):
-                #print(openlink.attrs["title"])
-                #print(openlink.text)
                 if project_url == "" and "href" in openlink.attrs and "zip" in openlink.text.lower().strip():
                     project_url = "https://openreview.net" + openlink.attrs["href"]
                 if pdf_url == "" and "href" in openlink.attrs and "title" in openlink.attrs and "pdf" in openlink.attrs["title"].lower().strip():
@@ -94,6 +107,18 @@ for link_index, l in enumerate(links):
     abstract = str(paper_soup.find("div", id="abstractExample").text).strip().strip("Abstract:").strip().replace("\n", " ")
     print(f"({link_index}/{len(links)}) [{paper_id}] Title: {paper_title}")
     # Write meta data to CSV
+    if resolving:  # Update paper URL in line
+        if pdf_url == "":
+            print(f"Failed to resolve PDF url for {paper_id}")
+            continue
+        with meta_csv_file.open('r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+        data[existing_ids.index(paper_id)][3] = pdf_url
+        with meta_csv_file.open('w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+    
     with meta_csv_file.open('a+', newline='') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         if paper_id in existing_ids:
