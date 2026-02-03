c9f57987ee482dd7b89ea329a9ac468411d5d616
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2026-02-03 18:06:10 +0100

    Update data enrichment

--- data_enrichment.py
+++ data_enrichment.py
@@ -16,11 +16,9 @@ scopus_api_key = yaml.safe_load(open("api_keys.yaml"))["scopus"]
 pybliometrics.init(keys=[scopus_api_key])
 
 
-titles = pd.read_csv(Path("reviews.csv"))["title"].to_list()
+df_reviews = pd.read_csv(Path("reviews.csv"))
 
-print(f"Querying {len(titles)} titles...")
-
-# TODO: Fix titles in unfindable.txt
+print(f"Querying {len(df_reviews)} titles...")
 
 enriched_output = Path("enriched_data.csv")
 unfindable_output = Path("unfindable.txt")
@@ -49,7 +47,7 @@ with enriched_output.open('a+', newline='') as myfile:
         cite_count_col = df.columns.to_list().index("citedby_count")
         already_done_titles = [s.lower() for s in df["title"].to_list()]
 
-# Some little stealing from https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
+# Some little borrowing from https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
 def deEmojify(text):
     import re
     regrex_pattern = re.compile(pattern = "["
@@ -60,31 +58,39 @@ def deEmojify(text):
                            "]+", flags = re.UNICODE)
     return regrex_pattern.sub(r'',text)
 
-# Titles that just won't rematch with their original
-bad_titles = [
-    "MMVQA: A Comprehensive Dataset for Investigating Multipage Multimodal Information Retrieval in PDF-based Visual Question Answering",
-    "DANETs: Deep Abstract Networks for Tabular Data Classification and Regression",
-    "‘Beach’ to ‘Bitch’: Inadvertent Unsafe Transcription of Kids’ Content on YouTube",
-    "FastAMI – a Monte Carlo Approach to the Adjustment for Chance in Clustering Comparison Metrics",
-    "RSPT: Reconstruct Surroundings and Predict Trajectory for Generalizable Active Object Tracking",
-    "“Allot?” Is “A Lot!” Towards Developing More Generalized Speech Recognition System for Accessible Communication",
-    "Fair Equilibria in Sponsored Search Auctions: The Advertisers' Perspective",
-    "Learning Discrete Representations via Constrained Clustering for Effective and Efficient Dense Retrieval (Extended Abstract)",
-    "GS2P: A Generative Pre-trained Learning to Rank Model with Over-parameterization for Web-Scale Search (Extended Abstract)",
-    "Topological Graph Neural Networks",
-    "Scaling Laws for Neural Machine Translation",
-    "Reversible Column Networks",
-    "KWIKBUCKS: CORRELATION CLUSTERING WITH CHEAP-WEAK AND EXPENSIVE-STRONG SIGNALS",
-    "QUANTIZED COMPRESSED SENSING WITH SCORE-BASED GENERATIVE MODELS",
-    "Multilinear Operator Networks",
-    "Mixture of LoRA Experts",
-    "Never Train from Scratch: Fair Comparison of Long-Sequence Models Requires Data-Driven Priors",
-    "Computing Unsatisfiable Cores for LTLf Specifications",
-]
 
-for title in titles:
-    # Testing example
-    #title = "Joint Human Pose Estimation and Instance Segmentation with PosePlusSeg"
+title_map = {  # These titles have 'typos' in them on SCOPUS!
+    "SVFI Spiking-Based Video Frame Interpolation for High-Speed Motion": "VFI: Spiking-Based Video Frame Interpolation for High-Speed Motion",
+    "Entropy Estimation via Normalizing Flow": "Entropy Eestimation via Normalizing Flow",
+    "Plurality Veto A Simple Voting Rule Achieving Optimal Metric Distortion": "PLURALITYVETO: A Simple Voting Rule Achieving Optimal Metric Distortion",
+    #"Align, Perturb and Decouple Toward Better Leverage of Difference Information for RSI Change Detection": ""  # Does not exists on scopus???
+    "Convergence in Multi-Issue Iterative Voting under Uncertainty": "Convergence of Multi-Issue Iterative Voting under Uncertainty",
+    "MA2CLMasked Attentive Contrastive Learning for Multi-Agent Reinforcement Learning": "MA2CL:Masked Attentive Contrastive Learning for Multi-Agent Reinforcement Learning",
+    "CostFormerCost Transformer for Cost Aggregation in Multi-view Stereo": "CostFormer: Cost Transformer for Cost Aggregation in Multi-view Stereo",
+    "Proving the Lottery Ticket Hypothesis for Convolutional Neural Networks": "PROVING THE STRONG LOTTERY TICKET HYPOTHESIS FOR CONVOLUTIONAL NEURAL NETWORKS",
+    # "Multi-Task Processes": "" # DOES NOT EXIST ON SCOPUS?
+    "Symbolic Learning to Optimize Towards Interpretability and Scalability": "SYMBOLIC LEARNING TO OPTIMIZE: TOWARDS IN-TERPRETABILITY AND SCALABILITY",
+    "Progressively Compressed Auto-Encoder for Self-supervised Representation Learning": "PROGRESSIVELY COMPRESSED AUTOENCODER FOR SELF-SUPERVISED REPRESENTATION LEARNING",
+    "$\mathrm{SE}(3)$-Equivariant Attention Networks for Shape Reconstruction in Function Space": "SE(3)-EQUIVARIANT ATTENTION NETWORKS FOR SHAPE RECONSTRUCTION IN FUNCTION SPACE",
+    "Clean-image Backdoor Attacking Multi-label Models with Poisoned Labels Only": "CLEAN-IMAGE BACKDOOR: ATTACKING MULTILABEL MODELS WITH POISONED LABELS ONLY",
+    "LightHGNN Distilling Hypergraph Neural Networks into MLPs for 100x Faster Inference": "LIGHTHGNN: DISTILLING HYPERGRAPH NEURAL NETWORKS INTO MLPS FOR 100× FASTER INFERENCE",
+    "pi2vec Policy Representation with Successor Features": "π2VEC: POLICY REPRESENTATION WITH SUCCESSOR FEATURES",
+    "Adversarial Robustness against Multiple and Single $l_p$-Threat Models via Quick Fine-Tuning of Robust Classifiers": "Adversarial Robustness against Multiple and Single lp-Threat Models via Quick Fine-Tuning of Robust Classifiers",
+    "Fast e-Approximation Algorithms for Binary Matrix Factorization": "Fast (1 + ε)-Approximation Algorithms for Binary Matrix Factorization",
+    #"An empirical analysis of compute-optimal large language model training": "INFERENCE SCALING LAWS: AN EMPIRICAL ANALYSIS OF COMPUTE-OPTIMAL INFERENCE FOR LLM PROBLEM-SOLVING"
+    "A High-Resolution Dataset for Instance Detection with Multi-View Object Capture": "A High-Resolution Dataset for Instance Detection with Multi-View Instance Capture",
+    "MultiVENT Multilingual Videos of Events and Aligned Natural Text": "MultiVENT: Multilingual Videos of Events with Aligned Natural Text",
+    "HEBO Pushing The Limits of Sample-Efficient Hyper-parameter Optimisation": "HEBO: Pushing The Limits of Sample-Efficient Hyperparameter Optimisation",
+    "A Scoping Study on AI Affordances in Early Childhood Education Mapping the Global Landscape Identifying Research Gaps and  Charting Future Research Directions": "A Scoping Study of AI Affordances in Early Childhood Education: Mapping the Global Landscape, Identifying Research Gaps, and Charting Future Research Directions",
+    #"Underspecification Presents Challenges for Credibility in Modern Machine Learning": "",
+    "MultiZoo and MultiBench A Standardized Toolkit for Multimodal Deep Learning": "MULTIZOO & MULTIBENCH: A Standardized Toolkit for Multimodal Deep Learning",
+}
+
+for index_row, row in df_reviews.iterrows():
+    title = row["title"]
+    year = row["year"]
+    source = row["source"]
+
     if title.lower() in already_done_titles:
         print(f"\tAlready present: {title}. Skipping.")
         continue
@@ -97,7 +103,11 @@ for title in titles:
         # Replace question mark, colons and exclamation as they cause issues with search
         search_title = search_title.replace("!", "").replace("?", "").replace(":", "").replace("^", "").replace(",", "")
         search_title = deEmojify(search_title)
-        s = ScopusSearch(f'TITLE("{search_title}")')
+        #print(search_title)
+        if search_title in title_map.keys():
+            #print(f"[!] Mapping search of {search_title} to {title_map[search_title]}")
+            search_title = title_map[search_title]
+        s = ScopusSearch(f'TITLE("{search_title}") AND PUBYEAR = {row["year"]} AND NOT TITLE("Extended Abstract")')
     except Exception as ex:
         print(f"Exception on title: {title}")
         print(ex)
@@ -115,7 +125,7 @@ for title in titles:
         continue
 
     if s.results is None:
-        print(f"Could not find: {title}")
+        print(f"Could not find: '{title}' (Search: '{search_title}')")
         print("Type citation count or anything else to skip: ", end="")
         c_count = input()
         try:
@@ -140,7 +150,8 @@ for title in titles:
         option_list = [{name: item for name, item in zip(option._fields, option)}
                        for option in s.results]
         for index, option in enumerate(option_list):
-            print(f"\t[{index}] {option['title']}")
+
+            print(f"\t[{index}] {option['title']} ({option['publicationName']})")
         print("Select the index which matches best, or s to skip: ", end="")
         selection = input()
         if selection == "s":
